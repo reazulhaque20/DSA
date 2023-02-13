4161defd8013f6e0c6de45b5376ee8a45161f6f7
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2023-02-13 16:45:44 +0600

    Array Search

--- ArraySearch/src/arraysearch/ArraySearch.java
+++ ArraySearch/src/arraysearch/ArraySearch.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package arraysory;
+package arraysearch;
 
 /**
  *
  * @author Reazul-System
  */
-public class ArraySory {
+public class ArraySearch {
 
     /**
      * @param args the command line arguments
@@ -27,13 +27,30 @@ public class ArraySory {
 	}//end outer for
 	return a;
     }
+    static void search(int a[], int x){
+	int sortArry[] = sort(a);
+	int len = sortArry.length, lo=0, hi=sortArry.length-1;
+	while(hi-lo>1){
+	    int mid = (hi+lo)/2;
+	    if(x>sortArry[mid]){
+		lo=mid+1;
+	    }else{
+		hi=mid;
+	    }
+	    if(sortArry[lo] == x){
+		System.out.println("Found At Index: "+lo);
+	    }else if(sortArry[hi] == x){
+		System.out.println("Found At Index: "+hi);
+	    }else{
+		System.out.println("Not Found");
+	    }
+	}
+    }
     public static void main(String[] args) {
 	// TODO code application logic here
 	int a[] = {2,5,3,6,7,1,9,4,8};
-	int sortArray[] = sort(a);
-	for(int b : sortArray){
-	    System.out.println("Sorted Array: "+b);
-	}
+	int x = 9;
+	search(a, x);
     }
     
 }
