2042f0e859c675f181fc8e4c10ab39c990500674
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2023-02-13 16:35:30 +0600

    Array Sort

---
+++ ArraySory/src/arraysory/ArraySory.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraysory;
+
+/**
+ *
+ * @author Reazul-System
+ */
+public class ArraySory {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int[] sort(int a[]){
+	int len=a.length, temp=0;
+	for(int i=0; i<len; i++){
+	    for(int j=0; j<len-i-1; j++){
+		if(a[j]>a[j+1]){
+		    temp = a[j+1];
+		    a[j+1] = a[j];
+		    a[j] = temp;
+		}//end if
+	    }//end inner for
+	}//end outer for
+	return a;
+    }
+    public static void main(String[] args) {
+	// TODO code application logic here
+	int a[] = {2,5,3,6,7,1,9,4,8};
+	int sortArray[] = sort(a);
+	for(int b : sortArray){
+	    System.out.println("Sorted Array: "+b);
+	}
+    }
+    
+}
